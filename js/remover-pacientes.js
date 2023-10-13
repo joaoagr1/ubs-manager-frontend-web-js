efe7b5013db525d18c7c3eb343ef1be629a4232a
joaoagr1 <joao.rollo1@gmail.com>
2023-10-13 14:01:09 -0300

    13-10-14:00

--- js/remover-pacientes.js
+++ js/remover-pacientes.js
@@ -9,11 +9,18 @@ var tabela = document.querySelector("table");
         var alvoEvento = event.target; //event.target seleciona o alvo afetado. enquanto que o this é o dono do evento.
         //como eu nao quero excluir o td e sim o tr eu chamo o pai do td com a função parentNode, jogo ela numa variavel e apago ela com . remove();
         var paiDoAlvo = alvoEvento.parentNode; 
+        var confirmacao = confirm("Tem certeza de que deseja excluir este elemento?");
+
+    if (confirmacao) {
         paiDoAlvo.classList.add("fadeOut"); //colocou a classe nele que vai ter essa transição ao remover
         setTimeout(function(){ //coloca um tempo antes de realizar a ação de remover 
             paiDoAlvo.remove();// para dar tempo da transição rodar
+            idExcluido = event.target.parentNode.querySelector(".info-id").textContent;
+            console.log(idExcluido);
+            
+
 
         },350);
 
-    });
+    }});
 
