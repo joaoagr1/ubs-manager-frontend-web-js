9181adb33811b552fb5c2feb7e467e773bc66861
joaoagr1 <joao.rollo1@gmail.com>
2023-10-14 02:05:44 -0300

    14.10.02:00

--- js/form.js
+++ js/form.js
@@ -5,68 +5,65 @@ function menssagem(event){
     event.preventDefault();
     
     var form = document.querySelector("#form-adiciona");
-
-   
     var pacienteobj = obtemPacienteFormulario(form);
+    nomeAdicionado = pacienteobj.nome;
+  //  console.log(nomeAdicionado);
+    emailAdicionado = pacienteobj.email;
+ //   console.log(pacienteobj.email);
+    crmAdicionado = pacienteobj.crm;
+   // console.log(pacienteobj.crm);
+    especialidadeAdicionado = pacienteobj.especialidade;
+   // console.log(pacienteobj.especialidade);
 
-    
+   // Dados a serem enviados no corpo da solicitação
+   const data = {
+    nome: nomeAdicionado,
+    email: emailAdicionado,
+    crm: crmAdicionado,
+    especialidade: especialidadeAdicionado
+  };
+  
+  
+  // URL de destino
+  const url = "http://localhost:8080/medicos";
+  
+  // Configuração da solicitação
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  };
+  
+  // Realiza a solicitação
+  fetch(url, options)
+    .then(response => {
+      if (response.status === 200) {
+        console.log("Requisição POST bem-sucedida");
+      } else {
+        console.error("Erro na requisição POST");
+      }
+    })
+    .catch(error => {
+      console.error("Erro na requisição: " + error);
+    });
+  
 
-    //cria a tr e a td do paciente
-    
-    // if (!validaPaciente(pacienteobj)) {
-    //     console.log("paciente inválido");
-    //     menssagemErro.textContent = "Paciente Inválido " + motivo;
-    //     form.reset();
-    //     return ;
-    // }
-
-    console.log(pacienteobj.nome);
-
-
-    //Adicionando o paciente na tabela
-    
     adicionaPacienteNaTabela(pacienteobj)
     form.reset();
-    //função do form que reseta os campos
 
-    
-
-// o form inteiro foi selecionado atravez do id e jogao dentro de uma variavel
-// só assim ent conseguimos acessar os campos desse form atravez dessa variavel
-
-//para inputs usamos values
-// para tags usamos textContent
 
     }
 
-
+    
     function adicionaPacienteNaTabela(pacienteobj){
         var pacienteTr = montaTr(pacienteobj);
         var tabela = document.querySelector("#tabela-pacientes");
         tabela.appendChild(pacienteTr);
-
-
     }
 
-    // function validaPaciente(pacienteobj) {
-    //     if(!validanome(pacienteobj.nome) && !validaemail(pacienteobj.email)){
-    //         motivo = "(nome e email)";
-    //         return false;
-    //     }
-        
-    //     if(!validanome(pacienteobj.nome) && validaemail(pacienteobj.email)){
-    //         motivo = "(nome)";
-    //         return false
-    //     }
-
-    //     if (validanome(pacienteobj.nome) && !validaemail(pacienteobj.email)) {
-    //         motivo = "(email)";
-    //         return false
-    //     }
-
-    //     return true;        
-    // }
-
+    
 
     
 
--- js/modal.js
+++ js/modal.js
@@ -12,7 +12,7 @@ if (exampleModal) {
     const modalTitle = exampleModal.querySelector('.modal-title')
     const modalBodyInput = exampleModal.querySelector('.modal-body input')
 
-    modalTitle.textContent = `New message to ${recipient}`
+   // modalTitle.textContent = `New message to ${recipient}`
     modalBodyInput.value = recipient
   })
 }
