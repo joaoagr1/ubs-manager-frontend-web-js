001412a409dc0c170decbd4b6111dcba593b8f37
joaoagr1 <joao.rollo1@gmail.com>
2023-10-21 00:15:12 -0300

    21.10.00:00

---
+++ js/alergias.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+let tabelaAlergias = document.querySelector("#tabelaAlergias");
+for (let i = 0; i < data.length; i++) {
+    let tr = insertRow();
+    let tdAlergias = tr.insertCell();
+
+    
+}
+
+});
