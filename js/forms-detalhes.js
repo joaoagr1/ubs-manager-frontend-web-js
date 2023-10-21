2c5ba70baf4b396fe378b16eb7062c1c00038953
joaoagr1 <joao.rollo1@gmail.com>
2023-10-21 14:41:24 -0300

    21.10.15:00

--- js/forms-detalhes.js
+++ js/forms-detalhes.js
@@ -36,3 +36,101 @@ fetch(url, requestOptions)
     console.error('Erro na requisição POST:', error);
   });
 });
+
+
+
+
+
+var botaoAdicionarMuc = document.querySelector("#botao-muc");
+var input = document.getElementById("campo-texto-muc");
+
+botaoAdicionarMuc.addEventListener("click", function() {
+    // Obtém o valor do campo input
+    console.log(input.value);
+
+    var mucAdicionada = input.value;
+
+    var identificador = localStorage.getItem("detalheStorage");
+
+    console.log(identificador)
+
+
+    
+    const url = `http://localhost:8080/muc/inserir?identificador=${identificador}&muc=${mucAdicionada}`;
+const requestOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    // Outros cabeçalhos, se necessário
+  }
+  // Você pode adicionar um corpo (body) aqui, se necessário
+};
+
+// Realiza a requisição Fetch
+fetch(url, requestOptions)
+  .then(response => {
+    if (response.ok) {
+      console.log('Requisição POST bem-sucedida.');
+      window.location.reload();
+
+    } else {
+      console.error('Erro na requisição POST.');
+    }
+  })
+  .catch(error => {
+    console.error('Erro na requisição POST:', error);
+  });
+});
+
+
+
+
+
+
+
+
+var botaoExcluirDetalhe = document.querySelector("#botao-excluir-detalhe")
+
+botaoExcluirDetalhe.addEventListener("click", function() {
+    // Coloque aqui o código a ser executado quando o botão for clicado
+
+    var identificador = localStorage.getItem("detalheStorage");
+
+
+    var confirmaExclusao = window.confirm("Você tem certeza de que deseja continuar?");
+    if (confirmaExclusao) {
+
+      fetch(`http://localhost:8080/medicos/${identificador}`, {
+  method: 'DELETE',
+  headers: {
+      'Content-Type': 'application/json'
+      // Outros cabeçalhos, se necessário
+  },
+})
+.then(response => {
+  if (response.ok) {
+      console.log(`O médico com ID ${identificador} foi excluído com sucesso.`);
+      window.location.href = "http://127.0.0.1:5503/index.html";
+
+  } else {
+      console.error(`Falha ao excluir o médico com ID ${identificador}.`);
+  }
+})
+.catch(error => {
+  console.error('Ocorreu um erro durante a exclusão:', error);
+});
+          
+
+
+    
+
+
+
+
+      console.log(dadosEdicao.idEdicao + "excluido")
+
+      
+    }
+
+});
+
