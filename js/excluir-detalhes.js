312d4d145977ad4258de561358239e003bdaae9b
joaoagr1 <joao.rollo1@gmail.com>
2023-10-23 19:54:39 -0300

    23.10.20:00

--- js/excluir-detalhes.js
+++ js/excluir-detalhes.js
@@ -1,4 +1,5 @@
-var tabelaAlergias = document.querySelector("#tabela-alergias")
+var tabelaAlergias = document.querySelector("#tabelaAlergias")
+var identificador = localStorage.getItem("detalheStorage");
 
 tabelaAlergias.addEventListener("dblclick", function (event) {
     alvoEvento = event.target;
@@ -8,7 +9,17 @@ tabelaAlergias.addEventListener("dblclick", function (event) {
     console.log(dadosEdicao.idEdicao + "chegou aqui")
 
     if (confirmacao) {
-        console.log(alvoEvento)
+        console.log("O item clicado foi "+alvoEvento.textContent+" e o identificador é "+identificador) 
 
     }
 });
+
+var tabelaMuc = document.querySelector("#tabelaMuc")
+
+tabelaMuc.addEventListener("dblclick", function (event){
+    alvoEvento = event.target;
+    paiDoAlvo = alvoEvento.parentNode;
+    var confirmacao = confirm("Tem certeza que deseja excluir este elemento?")
+    console.log(identificador + alvoEvento.textContent)
+
+})
