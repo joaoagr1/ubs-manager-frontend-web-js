6bb5543f46a84492221718cff9c14e2e4c15babf
joaoagr1 <joao.rollo1@gmail.com>
2023-10-23 23:59:09 -0300

    24.10.00:00

--- js/excluir-detalhes.js
+++ js/excluir-detalhes.js
@@ -9,7 +9,25 @@ tabelaAlergias.addEventListener("dblclick", function (event) {
     console.log(dadosEdicao.idEdicao + "chegou aqui")
 
     if (confirmacao) {
-        console.log("O item clicado foi "+alvoEvento.textContent+" e o identificador é "+identificador) 
+        console.log("O item clicado foi "+alvoEvento.textContent+" e o identificador é "+identificador)
+        
+        const url = `http://localhost:8080/alergias?identificador=${identificador}&alergias=${alvoEvento.textContent}`;
+
+
+        fetch(url, {
+            method: 'DELETE',
+          })
+            .then(response => {
+              if (response.ok) {
+                console.log('Requisição DELETE bem-sucedida');
+                location.reload(); 
+              } else {
+                console.error('Erro na requisição DELETE');
+              }
+            })
+            .catch(error => {
+              console.error('Erro ao realizar a requisição DELETE:', error);
+            });
 
     }
 });
@@ -22,4 +40,27 @@ tabelaMuc.addEventListener("dblclick", function (event){
     var confirmacao = confirm("Tem certeza que deseja excluir este elemento?")
     console.log(identificador + alvoEvento.textContent)
 
+
+
+    if (confirmacao) {
+        console.log("O item clicado foi "+alvoEvento.textContent+" e o identificador é "+identificador)
+        
+        const url =`http://localhost:8080/muc?identificador=${identificador}&muc=${alvoEvento.textContent}`;
+
+
+        fetch(url, {
+            method: 'DELETE',
+          })
+            .then(response => {
+              if (response.ok) {
+                console.log('Requisição DELETE bem-sucedida');
+                location.reload(); 
+              } else {
+                console.error('Erro na requisição DELETE');
+              }
+            })
+            .catch(error => {
+              console.error('Erro ao realizar a requisição DELETE:', error);
+            });
+        }
 })
